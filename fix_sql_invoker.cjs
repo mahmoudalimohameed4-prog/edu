5d09d7f7cdb8eeec33d889ef3694a1ae5f8748f1
hegazy <mahmoudalimohameed4@gmail.com>
2026-03-11 16:30:24 +0200

    chore: complete cleanup and final revert to local XAMPP setup

--- fix_sql_invoker.cjs
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-let file = 'eduswap_aiven.sql';
-let s = fs.readFileSync(file, 'utf8');
-s = s.replace(/SQL SECURITY DEFINER/g, 'SQL SECURITY INVOKER');
-fs.writeFileSync(file, s);
-console.log('Replaced SQL SECURITY DEFINER with INVOKER');
--- main.js
+++
@@ -1,7 +0,0 @@
-/**
- * Application Entry Point
- * This file imports the bootstrap function and initializes the application.
- */
-import bootstrap from "./app.bootstrap.js";
-bootstrap()
-
